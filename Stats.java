4f2fb9909cea30e0f6655a574fcdfcf39899afc3
Nate Langenkamp <langstore8@gmail.com>
2015-04-27 23:59:15 -0500

    Beginning of statz

--- Stats.java
+++ Stats.java
@@ -1,4 +1,8 @@
-public class Stats{
-	
-	
+Public class Stats{
+
+    int[] checkers = new int[normLanes + expressLanes];
+    for(int i = 0; i < normLanes + expressLanes; i++){
+        checkers[i] = lanes[i].total;
+    }
+
 }
